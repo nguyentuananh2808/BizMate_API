a89de06103b3a010f9b90ba2215160b32095bb6d
DESKTOP-NAQBUOF\Tuan Anh <anhh12318@gmail.com>
2025-08-01 00:22:50 +0700

    save

--- src/BizMate.Domain/Entities/OtpVerification.cs
+++ src/BizMate.Domain/Entities/OtpVerification.cs
@@ -1,11 +1,20 @@
-﻿namespace BizMate.Domain.Entities
+﻿
+namespace BizMate.Domain.Entities
 {
-    public class OtpVerification : BaseCoreEntity
+    public class OtpVerification 
     {
         public string Email { get; set; } = default!;
         public string OtpCode { get; set; } = default!;
         public DateTime ExpiredAt { get; set; }
         public bool IsUsed { get; set; }
+        public Guid Id { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public Guid? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; } = DateTime.UtcNow;
+        public Guid? UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
     }
 
 }
