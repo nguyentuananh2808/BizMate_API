2b5fbbd8d32038a9fc5c7c9da3461b6b231e1791
DESKTOP-NAQBUOF\Tuan Anh <anhh12318@gmail.com>
2025-10-01 01:17:08 +0700

    save

--- src/BizMate.Api/Program.cs
+++ src/BizMate.Api/Program.cs
@@ -126,7 +126,7 @@ internal class Program
         using (var scope = app.Services.CreateScope())
         {
             var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            db.Database.EnsureCreated();
+            db.Database.Migrate();
             await AppDbContextSeed.SeedAsync(db);
         }
 
